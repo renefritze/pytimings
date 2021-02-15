9c6bd655b7a81e6b6ccf4b88264f5eec8dcf9d41
René Fritze <rene.fritze@wwu.de>
2021-02-15 12:37:22 +0100

    dump code from https://github.com/wwu-numerik/scripts.git

---
+++ pytimings/plotting/mlmc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import itertools
+import matplotlib as mpl
+
+import pandas_common as pc
+
+def plot_mlmc(current, filename_base):
+    all = ['mlmc.all', 'fem.apply', 'msfem.Elliptic_MsFEM_Solver.apply']
+    all_labels = ['Overall', 'CgFem', 'MsFem', 'Ideal']
+    simple = ['mlmc.all']
+    for pref, tpl in {'all': (all, all_labels), 'simple': (simple, ['Overall', 'Ideal'])}.items():
+        cat, labels = tpl
+        ycols = ['{}_avg_wall_speedup'.format(v) for v in cat] + ['ideal_speedup']
+        pc.plot_common(current, '{}_{}'.format(pref, filename_base), ycols, labels)
+
+common_string = pc.common_substring(sys.argv[1:])
+merged = 'merged_{}.csv'.format(common_string)
+baseline_name = 'mlmc.all'
+
+header, current = pc.read_files(sys.argv[1:])
+headerlist = header['profiler']
+current = pc.sorted_f(current, True)
+# full = pc.speedup(headerlist, current.copy(), baseline_name)
+# full.transpose().to_csv(merged)
+current = pc.speedup(headerlist, current, baseline_name)
+# pprint(t_sections)
+plot_mlmc(current, merged)
+
+current.transpose().to_csv('filtered_' + merged)
---
+++ pytimings/plotting/msfem_scaleup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import logging
+import sys
+import traceback
+
+import pandas_common as pc
+
+common_string = pc.common_substring(sys.argv[1:])
+merged = 'merged_{}.csv'.format(common_string)
+baseline_name = 'msfem.all'
+
+def _get(func_name='scaleup'):
+    header, current = pc.read_files(sys.argv[1:])
+    headerlist = header['profiler']
+    current = pc.sorted_f(current, True)
+    func = getattr(pc, func_name)
+    current = func(headerlist, current, baseline_name)
+    current.transpose().to_csv('{}_{}'.format(merged, func_name))
+    return current
+
+current = _get('scaleup')
+pc.plot_msfem(current, merged, series_name='scaleup', xcol='grids.total_macro_cells')
+
+current = _get('speedup')
+pc.plot_msfem(current, merged, series_name='speedup', xcol='cores')
+
+try:
+    pc.plot_error(data_frame=current, filename_base='{}_errors'.format(merged),
+                  error_cols=['msfem_exact_H1s', 'msfem_exact_L2', 'fem_exact_H1s', 'fem_exact_L2'],
+                  xcol='grids.micro_cells_per_macrocell_dim', labels=['MsFEM $H^1_s$', 'MsFEM $L^2$', 'CgFEM $H^1_s$', 'CgFEM $L^2$'],
+                  baseline_name=baseline_name, logy_base=10,logx_base=2)
+except KeyError as k:
+    logging.error('No error data')
+    logging.error(traceback.format_exc())
+# current.transpose().to_excel(merged+'.xls')
+# current.to_csv(merged)
