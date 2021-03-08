b6284ff6f5224da3cdacf05da721cee8c5221caa
René Fritze <rene.fritze@wwu.de>
2021-03-08 16:14:20 +0100

    add a util function to output atexit

--- pytimings/tools.py
+++ pytimings/tools.py
@@ -1,4 +1,8 @@
+import atexit
 import os
+from functools import partial
+from pathlib import Path
+from typing import Union
 
 
 def ensure_directory_exists(dirname):
@@ -7,3 +11,14 @@ def ensure_directory_exists(dirname):
         os.makedirs(dirname)
     except FileExistsError:
         pass
+
+
+def output_at_exit(output_dir: Union[str, Path] = None, csv_base='timings', timings=None) -> None:
+    from pytimings.timer import global_timings
+
+    output_dir = output_dir or os.getcwd()
+    output_dir = Path(output_dir)
+    ensure_directory_exists(output_dir)
+    timings = timings or global_timings
+    output = partial(timings.output_per_rank, output_dir=output_dir, csv_base=csv_base)
+    atexit.register(output)
