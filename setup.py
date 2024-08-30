0257a460fa756cc70b89b16386c6433fb9d7a11a
René Fritze <rene.fritze@arup.com>
2024-08-30 14:44:23 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
-import sys
 import os
+import sys
+
+from setuptools import find_packages, setup
 
 sys.path.append(os.path.dirname(__file__))
 import dependencies
